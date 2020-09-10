3e55c57ffa460f94ea8452414453db993240c3ab
Magomed <megamagomed1@gmail.com>
2020-09-10 12:08:45 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+console.log(5 + +"4");
+
+let incr = 10,
+    decr = 10;
+console.log(++incr);
+console.log(incr++);
+console.log(8%3);
+console.log(2*4 == 8)
+
+const isChecked = false,
+      isClose = false,
+      a = 5
+
+
+console.log(!isChecked && !isClose && a ==5);
+console.log(isChecked || isClose|| a==5);
+
