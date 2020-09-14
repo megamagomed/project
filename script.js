0c568562a43cd66e13ac9ad734b30a7e916b2223
Magomed <megamagomed1@gmail.com>
2020-09-14 15:46:16 +0300

    урок 13 условия

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 "use strict";
 
-const numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", "");
+const num = 35;
+/*if (num < 49){
+    console.log("Error");
+} else if (num > 100){
+    console.log("Too much");
+} else {
+    console.log("Ok");
+}*/
+
+/*(num === 49) ? console.log("Ok"): console.log("error");*/
+
+switch (num){
+    case 49: console.log("adfsdf"); break;
+    case 100: console.log("adfsdf"); break;
+    case 50: console.log("Верно"); break;
+    default: console.log("не в этот раз"); break;
+}
+
+
+
+/*const numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", "");
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -16,7 +36,7 @@ const a = prompt("Один из последних просмотренных ф
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+console.log(personalMovieDB);*/
 
 
 
