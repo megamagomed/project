f611f360225cc4d477f9d76a37aa26381f260d8a
Magomed <megamagomed1@gmail.com>
2020-09-15 14:44:44 +0300

    14 урок циклы

--- script.js
+++ script.js
@@ -1,44 +1,28 @@
 "use strict";
 
-const num = 35;
-/*if (num < 49){
-    console.log("Error");
-} else if (num > 100){
-    console.log("Too much");
-} else {
-    console.log("Ok");
+let num = 50;
+
+/*
+while (num < 55){
+    console.log(num);
+    num++;
 }*/
 
-/*(num === 49) ? console.log("Ok"): console.log("error");*/
+/*
+do {
+    console.log(num);
+    num++;
 
-switch (num){
-    case 49: console.log("adfsdf"); break;
-    case 100: console.log("adfsdf"); break;
-    case 50: console.log("Верно"); break;
-    default: console.log("не в этот раз"); break;
 }
+while (num<55);*/
 
+let i;
 
+for(i=1;i<10;i++){
+    if (i === 6){
+        //break;
+        continue;
+    }
+    console.log(i);
 
-/*const numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", "");
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-      b = prompt("На сколько оцените его?", ''),
-      c = prompt("Один из последних просмотренных фильмов?", ""),
-      d = prompt("На сколько оцените его?", '');
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);*/
-
-
-
-
-
+}
