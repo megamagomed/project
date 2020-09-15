989bf8ebab073fb4deb233c8868b5f1b3d6a4f61
Magomed <megamagomed1@gmail.com>
2020-09-15 17:30:13 +0300

    15 урок практика по циклам и условиям

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
 "use strict";
 
-let num = 50;
+const numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", "");
 
-/*
-while (num < 55){
-    console.log(num);
-    num++;
-}*/
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-/*
-do {
-    console.log(num);
-    num++;
-
+let i=0;
+while (i<2){
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+          b = prompt("На сколько оцените его?", '');
+    if(a!= null && b!= null && a!="" && b!="" && a.length < 50){
+        personalMovieDB.movies[a] = b;
+        console.log(" Done");
+        i++;
+        } else{
+        console.log("Error");
+        }
 }
-while (num<55);*/
 
-let i;
+if(personalMovieDB.count<10){
+    console.log("Просмотрено довольно мало фильмов");
+} else if (10<= personalMovieDB.count <=30){
+    console.log("Вы классический зритель");
+} else console.log("произошла ошибка");
 
-for(i=1;i<10;i++){
-    if (i === 6){
-        //break;
-        continue;
-    }
-    console.log(i);
-
-}
+console.log(personalMovieDB);
