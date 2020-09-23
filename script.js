664caa50fd8dc3448d6e846fb979c85fd3e8273d
Magomed <megamagomed1@gmail.com>
2020-09-23 15:49:52 +0300

    Урок 19. Массивы и псевдомассивы

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
 "use strict";
 
-function first() {
-    //Do something
-    setTimeout(function () {
-        console.log(1);
-    }, 500)
+const arr = [1, 2, 3, 6, 8];
 
-}
-first();
-function second() {
-    console.log(2);
+/*arr[99] = 0;
+console.log(arr);
+console.log(arr.length);*/
 
-}
-second();
-function learnJS(lang, callback) {
-    console.log(`Я учу: ${lang}`);
-    callback();
-}
-learnJS("JavaScript", function () {
-    console.log("Я прошел этот урок!");
+arr.forEach(function (item, i, arr){
+    console.log(`${i}: ${item} внутри массива ${arr}`)
 });
+
+
+/*
+arr.pop();
+console.log(arr);
+arr.push(8);
+
+console.log(arr);
+for (let i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+}
+for(let value of arr){
+    console.log(value);
+}*/
+
+const str = prompt("", "");
+//const products = str.split(", ");
+//console.log(products.join("; "));
+//products.sort();
+//console.log(products)
+
